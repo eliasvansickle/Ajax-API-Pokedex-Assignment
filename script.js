b30f191603952ce523dd975cbb0a4f2a89aa4642
eliasvansickle <eliasvansickle@gmail.com>
2015-06-09 14:10:12 -0700

    Pokedex sidebar in place moving on to functionality

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
 	var imgUrl = function() {
 	for (var i = 1; i < 152; i++) {
-		$("#pokemon").append('<img src="http://pokeapi.co/media/img/'+i+'.png">');
+		$("#pokemon").append('<img id="'+i+'" src="http://pokeapi.co/media/img/'+i+'.png">');
 	}
 };
 imgUrl();
+
+
+
+
+
+
+
+
+
 });
