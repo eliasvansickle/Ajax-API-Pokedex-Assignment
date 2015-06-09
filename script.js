6a72ba22180610210b179331a62d5cd88ad031a7
eliasvansickle <eliasvansickle@gmail.com>
2015-06-09 15:51:21 -0700

    function ready, about to start adding content when image is clicked

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-$(document).ready(function() {
-	var imgUrl = function() {
+var imgUrl = function() {
 	for (var i = 1; i < 152; i++) {
 		$("#pokemon").append('<img id="'+i+'" src="http://pokeapi.co/media/img/'+i+'.png">');
-	}
+
+	};
 };
-imgUrl();
+
+var data = function() {
+			var id = "";
+			$("img").click(function() {
+				id = $(this).attr("id");
+				$.get("http://pokeapi.co/api/v1/pokemon/"+id+"/", function(res) {
+					
+				}, "json");
 
 
 
+	});
+};
 
 
 
-
-
-
+$(document).ready(function() {
+	imgUrl();
+	data();
 });
+
+
